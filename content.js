3659f328ddca1909ff98a0d353ca3a867ef70e9d
anushreddy <anushreddy04@gmail.com>
2017-09-02 13:59:59 -0500

    updated content.js

--- content.js
+++ content.js
@@ -5,15 +5,32 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // Collect the necessary data 
     // (For your specific requirements `document.querySelectorAll(...)`
     //  should be equivalent to jquery's `$(...)`)
-    //var jobtitle = document.querySelectorAll('h1');
-var jobInfo = {
+    var title = "";
+    var company = "";
+    var location = "";
+
+    $('.jobRow.row a.jobTitle').each(function(){
+      title += $(this).text() + "<br>";
+    }) 
+    $('.jobRow.row span.jobCompany').each(function(){
+        company += $(this).text() + "<br>";
+    })  
+    $('.jobRow.row span.jobLocation').each(function(){
+        location += $(this).text() + "<br>";
+    })   
+
+    console.log(title);console.log(company);console.log(location);
+/*var jobInfo = {
     title: $('.jobRow.row a.jobTitle').text(),
     company: $('.jobRow.row span.jobCompany').text(),
     location:  $('.jobRow.row span.jobLocation').text()
+};*/
+
+var jobInfo = {
+    title: title,
+    company: company,
+    location: location
 };
-    console.log(jobInfo.title);
-    console.log(jobInfo.company);
-    console.log(jobInfo.location);
     // Directly respond to the sender (popup), 
     // through the specified callback */
     sendResponse(jobInfo);
